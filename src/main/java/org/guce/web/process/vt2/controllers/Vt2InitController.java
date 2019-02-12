231e6d1f4a849e233f74a2be116536c327356613
Marius YENKE MBEUYO <yenkemarius@yahoo.fr>
2019-02-12 16:17:04 +0100

    Renommage du code de type de piece jointe relatif a l'attestation d'inscription au registre de commerce

--- src/main/java/org/guce/web/process/vt2/controllers/Vt2InitController.java
+++ src/main/java/org/guce/web/process/vt2/controllers/Vt2InitController.java
@@ -322,7 +322,7 @@ public class Vt2InitController extends WebGuceDefaultController {
                 JsfUtil.addErrorMessage(bundle("PleaseAddOneGood"));
                 return;
             }
-            if (!validRequiredAttachements(getProcessParam(Vt2Constant.PROCESS_PARAM_REQUIRED_ATTACHMENT, "FACTURE_PROFORMA,DEMANDE_TIMBRE,ATTESTATION_INSCRIPTION_RC,NOTE_TECHNIQUE,QUITTANCE"))) {
+            if (!validRequiredAttachements(getProcessParam(Vt2Constant.PROCESS_PARAM_REQUIRED_ATTACHMENT, "FACTURE_PROFORMA,DEMANDE_TIMBRE,ATTESTATION_INSCRIPTION,NOTE_TECHNIQUE,QUITTANCE"))) {
                 return;
             }
         } catch (Exception ex) {
